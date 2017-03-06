17071200a2395a0799e4b3679bc2ae282109a879
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-03-06 19:30:02 +0100

    Add books list with CRUD

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,67 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = set_book(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+    @book = set_book(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @book = set_book(params[:id])
+
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @book = set_book(params[:id])
+
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_book(book_id)
+      # @book = Book.find(params[:id])
+      Book.find(book_id)
+    end
+
+    def book_params
+      params.require(:book).permit(:name, :grade, :author, :description)
+    end
+end
---
+++ spec/factories/books.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :book do
+    name "MyString"
+    grade "MyString"
+    author "MyString"
+    description "MyString"
+  end
+end
