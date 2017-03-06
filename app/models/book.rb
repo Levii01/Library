9d9e1862fde90d49442807c5f0f86ae78f646528
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-03-06 21:04:32 +0100

    Create relation between books and user in borrow_book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,7 @@
-
-
-
-
 class Book < ApplicationRecord
+  has_many :users, through: :borrow_books
+  has_many :borrow_books
+
   validates :name, uniqueness: true, presence:true
   validates :author, presence:true
 end
