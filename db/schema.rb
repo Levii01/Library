b0a4baf437e0d4c97e23fdb22e2b34370fb3e9dd
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-03-06 20:54:02 +0100

    Create model borrow books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306190033) do
+ActiveRecord::Schema.define(version: 20170306194616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20170306190033) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["title"], name: "index_books_on_title", using: :btree
+  end
+
+  create_table "borrow_books", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "book_id"
+    t.boolean  "give_back",  default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.index ["book_id"], name: "index_borrow_books_on_book_id", using: :btree
+    t.index ["user_id"], name: "index_borrow_books_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +52,6 @@ ActiveRecord::Schema.define(version: 20170306190033) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "borrow_books", "books"
+  add_foreign_key "borrow_books", "users"
 end
---
+++ spec/factories/borrow_books.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :borrow_book do
+    user nil
+    book nil
+  end
+end
