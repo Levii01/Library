b62c5f303ac6b46d9d0a17d58afca47059b0ca8b
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-03-07 12:53:29 +0100

    Refactor borrow controller

--- app/controllers/borrow_books_controller.rb
+++ app/controllers/borrow_books_controller.rb
@@ -4,10 +4,10 @@ class BorrowBooksController < ApplicationController
   def borrow_new
     book = Book.find(params[:id])
     if BorrowBook.exists?(user: current_user, book: book, give_back: false)
-      redirect_to books_path(params[:id]), notice: "You haven't given back this book yet "
+      redirect_to books_path, notice: "You haven't given back this book yet "
     else
       BorrowBook.create(user: current_user, book: book)
-      redirect_to books_path(params[:id]), notice: "Book #{book.title} has been borrowed"
+      redirect_to books_path, notice: "Book #{book.title} has been borrowed"
     end
   end
 
@@ -15,9 +15,9 @@ class BorrowBooksController < ApplicationController
     book = Book.find(params[:id])
     if BorrowBook.exists?(user: current_user, book: book, give_back: false)
       BorrowBook.find_by(user: current_user, book: book, give_back: false).update!(give_back: true)
-      redirect_to my_books_path(params[:id]), notice: "Book #{book.title} has given back"
+      redirect_to my_books_path, notice: "Book #{book.title} has given back"
     else
-      redirect_to my_books_path(params[:id]), notice: "You don't have this book"
+      redirect_to my_books_path, notice: "You don't have this book"
     end
   end
 
