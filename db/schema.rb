23acb41aa7d35627bd80758fba71e9441a26801d
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-03-08 18:38:12 +0100

    Migration which change name table, borrowbook > userbook

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307153047) do
+ActiveRecord::Schema.define(version: 20170308173417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,14 +25,14 @@ ActiveRecord::Schema.define(version: 20170307153047) do
     t.index ["title"], name: "index_books_on_title", using: :btree
   end
 
-  create_table "borrow_books", force: :cascade do |t|
+  create_table "user_books", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "book_id"
     t.boolean  "give_back",  default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-    t.index ["book_id"], name: "index_borrow_books_on_book_id", using: :btree
-    t.index ["user_id"], name: "index_borrow_books_on_user_id", using: :btree
+    t.index ["book_id"], name: "index_user_books_on_book_id", using: :btree
+    t.index ["user_id"], name: "index_user_books_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,6 +56,6 @@ ActiveRecord::Schema.define(version: 20170307153047) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "borrow_books", "books"
-  add_foreign_key "borrow_books", "users"
+  add_foreign_key "user_books", "books"
+  add_foreign_key "user_books", "users"
 end
