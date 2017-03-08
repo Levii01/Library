97b6c1fd9498e7bca480ee4365c68f06d7499b5c
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-03-08 18:19:56 +0100

    Delete action destroy user from devise

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,15 @@ Rails.application.routes.draw  do
   get 'my_books', to: 'users#index'
   root to: 'home#index'
 
-  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+  devise_for :users, skip: :registrations, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+  devise_scope :user do
+    resource :registration,
+      only: [:new, :create, :edit, :update],
+      path: 'users',
+      path_names: { new: 'sign_up' },
+      controller: 'devise/registrations',
+      as: :user_registration do
+        get :cancel
+      end
+  end
 end
