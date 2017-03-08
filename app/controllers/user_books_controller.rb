7624d68bbee93765f55c79a0772e9b88262b7563
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-03-09 00:57:36 +0100

    Fix user_books controller

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -14,7 +14,7 @@ class UserBooksController < ApplicationController
   def give_back
     book = Book.find(params[:id])
     if UserBook.exists?(user: current_user, book: book, give_back: false)
-      UserBook.find_by(user: current_user, book: book, give_back: false).update!(give_back: false)
+      UserBook.find_by(user: current_user, book: book, give_back: false).update!(give_back: true )
       redirect_to my_books_path, notice: "Book #{book.title} has given back."
     else
       redirect_to my_books_path, notice: "You don't have this book."
